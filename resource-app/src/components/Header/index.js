16181ae25196d693755b8633df95f6488bbd6b19
E-K8 <ekatplat@gmail.com>
2022-01-18 11:39:20 +0000

    add components, display Header and Main

---
+++ resource-app/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <header>
+      <img src="https://www.schoolofcode.co.uk/static/logo-51c754388b198e5bbb0d08a971ebbfa2.png" />
+      <nav></nav>
+    </header>
+  );
+}
---
+++ resource-app/src/components/Main/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Main() {
+  return (
+    <main>
+      <h1>Resource API</h1>
+    </main>
+  );
+}
