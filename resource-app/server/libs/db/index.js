4eb477bcae53afdf2e25ef36a9999a032abf91b3
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-01-18 18:25:34 +0000

    created resource table by hardcoding Heroku string

--- resource-app/server/libs/db/index.js
+++ resource-app/server/libs/db/index.js
@@ -1,13 +1,13 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  connectionString:process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
 
 export default function query(text, params) {
--- resource-app/server/libs/db/scripts/createResourceTable.js
+++ resource-app/server/libs/db/scripts/createResourceTable.js
@@ -3,16 +3,17 @@ import query from "../index.js";
 async function createResourceTable() {
   await query(`CREATE TABLE IF NOT EXISTS resources (
     resourceID SERIAL PRIMARY KEY, 
-    title VARCHAR(255)
+    title VARCHAR(255),
+    topic VARCHAR(255),
+    resource_type VARCHAR(255),
+    week VARCHAR(255),
+    thumbnail VARCHAR(255),
+    url VARCHAR(500),
+    description VARCHAR(255)
 )`);
   console.log("Created Resource Table!");
 }
 
 createResourceTable();
 
-// topic VARCHAR(255),
-// resource_type VARCHAR(255),
-// week VARCHAR(255),
-// thumbnail VARCHAR(255),
-// url VARCHAR(500),
-// description VARCHAR(255)
+
