044690fbce2b97bc08a0639d47526e4f565cc6f6
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-01-18 17:28:38 +0000

    received The server does not support SSL connections error mess

--- resource-app/server/libs/db/scripts/createResourceTable.js
+++ resource-app/server/libs/db/scripts/createResourceTable.js
@@ -1,17 +1,18 @@
-import query from "..";
-import resources from "../resource-data";
+import query from "../index.js";
 
 async function createResourceTable() {
-  await query(`CREATE TABLE IF NOT EXISTS Resources (
-    ResourceID SERIAL PRIMARY KEY, 
-    title VARCHAR(255),
-    topic VARCHAR(255),
-    resource_type VARCHAR(255),
-    week VARCHAR(255),
-    thumbnail VARCHAR(255),
-    url VARCHAR(500),
-    description VARCHAR(255)
+  await query(`CREATE TABLE IF NOT EXISTS resources (
+    resourceID SERIAL PRIMARY KEY, 
+    title VARCHAR(255)
 )`);
+  console.log("Created Resource Table!");
 }
 
 createResourceTable();
+
+// topic VARCHAR(255),
+// resource_type VARCHAR(255),
+// week VARCHAR(255),
+// thumbnail VARCHAR(255),
+// url VARCHAR(500),
+// description VARCHAR(255)
