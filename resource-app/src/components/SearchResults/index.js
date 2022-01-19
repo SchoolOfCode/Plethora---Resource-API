a227790026a4ca7c32ecce81f6cf29b6542007a1
E-K8 <ekatplat@gmail.com>
2022-01-19 16:18:10 +0000

    add useState, useEffect to SearchResults, amend env file, not working yet

--- resource-app/src/components/SearchResults/index.js
+++ resource-app/src/components/SearchResults/index.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 export function SearchResults() {
-  return <div>Search Results will be here</div>;
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function getResults() {
+      const response = await fetch(`${API_URL}`);
+      const data = await response.json();
+      if (data.success === true) {
+        console.log(data);
+        setResults(data.payload);
+      }
+    }
+    getResults();
+  }, []);
+
+  // return <div>Search Results will be here</div>;
+
+  return (
+    <ul>
+      {results.map(function (result) {
+        return (
+          <li key={result.resourceid}>
+            <p>Title: {result.title}</p>
+            <p>URL: {result.url}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
