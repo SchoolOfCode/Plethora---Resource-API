393f0b0d27ccb8e79e5c4e2569a58dec99376429
E-K8 <ekatplat@gmail.com>
2022-01-20 16:19:13 +0000

    userInput and search returns filtered results

--- resource-app/src/components/Main/index.js
+++ resource-app/src/components/Main/index.js
@@ -2,36 +2,32 @@ import React, { useEffect, useState } from "react";
 import "./Main.css";
 import { Description } from "../Description";
 import { Input } from "../Input";
-import { SearchResults } from "../SearchResults";
+// import { SearchResults } from "../SearchResults";
 
-const API_URL = "https://w9project.herokuapp.com/resources";
+const API_URL = "https://w9project.herokuapp.com";
 
 export function Main() {
-   const [results, setResults] = useState([]);
-   const [userInput, setUserInput] = useState("");
+  const [results, setResults] = useState([]);
 
-   useEffect(() => {
-      async function getResults() {
-         if (userInput === "") {
-            alert("Please do something productive with me!");
-            return;
-         }
-         const response = await fetch(`${API_URL}?title=${userInput}`);
-         const data = await response.json();
+  async function getResults(text) {
+    if (text === "") {
+      alert("Please do something productive with me!");
+      return;
+    }
+    const response = await fetch(`${API_URL}/resources?title=${text}`);
+    const data = await response.json();
 
-         console.log(data);
+    console.log(data);
 
-         setResults(data);
-      }
-      getResults();
-   }, [userInput]);
+    setResults(data);
+  }
 
-   return (
-      <main>
-         <h1 className="title">Resource API</h1>
-         <Description />
-         <Input />
-         <SearchResults />
-      </main>
-   );
+  return (
+    <main>
+      <h1 className="title">Resource API</h1>
+      <Description />
+      <Input handleSubmit={getResults} />
+      {/* <SearchResults /> */}
+    </main>
+  );
 }
