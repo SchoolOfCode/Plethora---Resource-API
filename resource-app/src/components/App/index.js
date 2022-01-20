41aa6746a32720aa07e9da555044dab390c64be9
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-01-20 10:44:58 +0000

    test

--- resource-app/src/components/App/index.js
+++ resource-app/src/components/App/index.js
@@ -3,12 +3,13 @@ import { Main } from "../Main";
 import { Header } from "../Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
-  );
+   return (
+      <div className="App">
+         <Header />
+         <Main />
+      </div>
+   );
 }
 
 export default App;
+//hello
