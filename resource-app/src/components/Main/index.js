cb8f5554615576c3fce860a6368687e8f968ca04
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-01-20 14:45:13 +0000

    query done

--- resource-app/src/components/Main/index.js
+++ resource-app/src/components/Main/index.js
@@ -1,16 +1,37 @@
-import React from "react";
-import './Main.css'
+import React, { useEffect, useState } from "react";
+import "./Main.css";
 import { Description } from "../Description";
 import { Input } from "../Input";
 import { SearchResults } from "../SearchResults";
 
+const API_URL = "https://w9project.herokuapp.com/resources";
+
 export function Main() {
-  return (
-    <main>
-      <h1 className="title">Resource API</h1>
-      <Description />
-      <Input />
-      <SearchResults />
-    </main>
-  );
+   const [results, setResults] = useState([]);
+   const [userInput, setUserInput] = useState("");
+
+   useEffect(() => {
+      async function getResults() {
+         if (userInput === "") {
+            alert("Please do something productive with me!");
+            return;
+         }
+         const response = await fetch(`${API_URL}?title=${userInput}`);
+         const data = await response.json();
+
+         console.log(data);
+
+         setResults(data);
+      }
+      getResults();
+   }, [userInput]);
+
+   return (
+      <main>
+         <h1 className="title">Resource API</h1>
+         <Description />
+         <Input />
+         <SearchResults />
+      </main>
+   );
 }
--- resource-app/src/components/SearchResults/index.js
+++ resource-app/src/components/SearchResults/index.js
@@ -1,43 +1,32 @@
-// import React, { useEffect, useState } from "react";
-import { useEffect } from "react";
+// // import React, { useEffect, useState } from "react";
 
-// const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "https://w9project.herokuapp.com/resources";
+// // const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = "https://w9project.herokuapp.com/resources";
 
-export function SearchResults() {
-  // const [results, setResults] = useState([]);
+// export function SearchResults() {
+//   // const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    async function getResults() {
-      const response = await fetch(`${API_URL}`, {
-        // method: 'GET',
-        // headers: {
-        //   'Content-Type': 'application/json',
-        // }
-      });
-      const data = await response.json();
-      // if (data.success === true) {
-      //   console.log(data);
-      //   setResults(data.payload);
-      // }
-      console.log(data);
-    }
-    getResults();
-  }, []);
+//   useEffect(() => {
+//     async function getResults() {
+//       const response = await fetch(`${API_URL}`)
+//       const data = await response.json();
 
-  // return <div>Search Results will be here</div>;
+//       console.log(data);
+//     }
+//     getResults();
+//   }, []);
 
-  return (
-    <ul>
-      <li>Test</li>
-  {/* //     {results.map(function (result) { */}
-  {/* //       return (
-  //         <li key={result.resourceid}>
-  //           <p>Title: {result.title}</p>
-  //           <p>URL: {result.url}</p>
-  //         </li>
-  //       );
-  //     })} */}
-    </ul>
-  );
-}
+//   return (
+//     <ul>
+//       <li>Test</li>
+//   {/* //     {results.map(function (result) { */}
+//   {/* //       return (
+//   //         <li key={result.resourceid}>
+//   //           <p>Title: {result.title}</p>
+//   //           <p>URL: {result.url}</p>
+//   //         </li>
+//   //       );
+//   //     })} */}
+//     </ul>
+//   );
+// }
